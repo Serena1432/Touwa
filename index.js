c628d44dcd677261872f8ef7f0ee698c1e783e93
Meir <nicolevianth@gmail.com>
2021-07-18 15:33:08 +0700

    Added the /user endpoint

--- index.js
+++ index.js
@@ -561,6 +561,193 @@ app.get("/tag/:tag", function(req, res) {
 	});
 });
 
+
+/*
+-----------------------------------------------------------------
+	Getting information of an user
+-----------------------------------------------------------------
+*/
+app.get("/user/:id", function(req, res) {
+	// Add some headers
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Content-Type", "application/json");
+	// Declaring the time and URL variable
+	var time = new Date(), url = domain + "/user-" + encodeURIComponent(req.params.id);
+	// Getting content from the website
+	request(url, function(error, response, body) {
+		// Returning error if have
+		if (error) return res.status(500).send({success: false, error_code: 500, message: error.toString(), url: url, documentation_url: "https://www.github.com/LilShieru/Touwa/wiki"});
+		if (response.statusCode != 200) return res.status(503).send({success: false, error_code: 503, message: "The target website returned a " + response.statusCode + " error.", url: url, documentation_url: "https://www.github.com/LilShieru/Touwa/wiki"});
+		// Declaring the ping variable
+		var ping = new Date().getTime() - time.getTime();
+		// Parsing content
+		const dom = new JSDOM(body), document = dom.window.document;
+		try {
+			if (document.title.includes("404")) return res.status(404).send({success: false, error_code: 404, message: "The target website returned a 404 error.", url: url, documentation_url: "https://www.github.com/LilShieru/Touwa/wiki"});
+			if (document.getElementsByClassName("block-top").length > 0) return res.status(404).send({success: false, error_code: 404, message: "The target website returned a 404 error.", url: url, documentation_url: "https://www.github.com/LilShieru/Touwa/wiki"});
+			var data = {
+				user: {
+					username: document.getElementsByClassName("wall-name")[0].getElementsByTagName("h2")[0].textContent,
+					main_badge: {
+						image_url: document.getElementsByClassName("wall-name")[0].getElementsByTagName("p")[0].getElementsByTagName("img")[0].src,
+						name: document.getElementsByClassName("wall-name")[0].getElementsByTagName("p")[0].getElementsByTagName("b")[0].textContent,
+						description: document.getElementsByClassName("wall-name")[0].getElementsByTagName("p")[1].textContent
+					},
+					avatar_url: document.getElementsByClassName("wall-avatar")[0].getElementsByTagName("img")[0].src.includes("https://") ? document.getElementsByClassName("wall-avatar")[0].getElementsByTagName("img")[0].src : (domain + document.getElementsByClassName("wall-avatar")[0].getElementsByTagName("img")[0].src),
+					forum_link: domain + "/forum/user-" + req.params.id
+				},
+				waifus: [],
+				uploaded_comics: [],
+				latest_comments: []
+			};
+			var info = document.getElementsByClassName("info-row");
+			for (var i = 0; i < info.length; i++) {
+				switch (info[i].getElementsByClassName("col-1")[0].textContent) {
+					case "Trưởng nhóm:": {
+						data.user.group = {
+							name: info[i].getElementsByClassName("col-2")[0].getElementsByTagName("a")[0].textContent,
+							link: domain + info[i].getElementsByClassName("col-2")[0].getElementsByTagName("a")[0].href,
+							leader: true
+						};
+						break;
+					}
+					case "Thành viên nhóm:": {
+						data.user.group = {
+							name: info[i].getElementsByClassName("col-2")[0].getElementsByTagName("a")[0].textContent,
+							link: domain + info[i].getElementsByClassName("col-2")[0].getElementsByTagName("a")[0].href,
+							leader: false
+						};
+						break;
+					}
+					case "Giới tính:": {
+						data.user.gender = info[i].getElementsByClassName("col-2")[0].textContent;
+						break;
+					}
+					case "Ngày sinh:": {
+						data.user.birth_date = info[i].getElementsByClassName("col-2")[0].textContent;
+						break;
+					}
+					case "Gia nhập:": {
+						var joined = info[i].getElementsByClassName("col-2")[0].textContent.split(" - ");
+						data.user.joined = {
+							date: joined[1],
+							time: joined[0]
+						};
+						break;
+					}
+					case "Đã bình luận:": {
+						data.user.comments = parseInt(info[i].getElementsByClassName("col-2")[0].textContent);
+						break;
+					}
+					case "Được thích:": {
+						data.user.likes = parseInt(info[i].getElementsByClassName("col-2")[0].textContent);
+						break;
+					}
+					case "Yên:": {
+						data.user.yen = parseInt(info[i].getElementsByClassName("col-2")[0].textContent);
+						break;
+					}
+					case "Danh Hiệu:": {
+						data.user.badges_list_link = domain + "/forum/huy_hieu.php?id_user=" + req.params.id;
+						data.user.badges = [];
+						var badges = info[i].getElementsByClassName("col-2")[0].getElementsByTagName("img");
+						for (var j = 0; j < badges.length; j++) {
+							var name = badges[j].title.split(" - ");
+							data.user.badges.push({
+								name: name[0],
+								description: name[1],
+								icon_url: badges[j].src
+							});
+						}
+						break;
+					}
+					case "Giới thiệu:": {
+						data.user.introduction = info[i].getElementsByClassName("col-2")[0].textContent;
+						break;
+					}
+				}
+			}
+			var box = document.getElementsByClassName("left-info");
+			for (var i = 1; i < box.length; i++) {
+				switch (box[i].getElementsByClassName("bar-title-blue")[0].textContent) {
+					case "Waifu": {
+						var waifus = box[i].getElementsByClassName("item");
+						for (var j = 0; j < waifus.length; j++) {
+							data.waifus.push({
+								name: waifus[j].getElementsByTagName("div")[1].getElementsByTagName("b")[0].textContent,
+								current_level: parseInt(waifus[j].getElementsByTagName("div")[1].getElementsByTagName("b")[1].textContent.substr(7, 1)),
+								max_level: parseInt(waifus[j].getElementsByTagName("div")[1].getElementsByTagName("b")[1].textContent.substr(9)),
+								hearts: parseInt(waifus[j].getElementsByTagName("div")[1].getElementsByTagName("b")[2].textContent.substr(1)),
+								description: waifus[j].getElementsByTagName("div")[1].getElementsByTagName("span")[0].textContent
+							});
+						}
+						break;
+					}
+					case "Truyện đã upload": {
+						var comics  = box[i].getElementsByClassName("item");
+						for (var x = 0; x < comics.length; x++) {
+							var comic = comics[x], comicLink = comic.getElementsByClassName("box-description")[0].getElementsByTagName("a")[0].href.substr(1);
+							var comicData = {
+								name: comic.getElementsByClassName("box-description")[0].getElementsByTagName("a")[0].textContent,
+								id: parseInt(comicLink.substr(0, comicLink.indexOf("-"))),
+								link: domain + comic.getElementsByClassName("box-description")[0].getElementsByTagName("a")[0].href,
+								chapter_status: comic.getElementsByClassName("box-description")[0].getElementsByTagName("h2")[0].textContent.substr(comic.getElementsByClassName("box-description")[0].getElementsByTagName("a")[0].textContent.length + 4),
+								other_names: [],
+								tags: []
+							};
+							var info = comic.getElementsByTagName("p");
+							for (var j = 0; j < info.length; j++) {
+								switch (info[j].getElementsByTagName("b")[0].textContent) {
+									case "Tên Khác: ": {
+										for (var k = 0; k < info[j].getElementsByTagName("span").length; k++) comicData.other_names.push({
+											name: info[j].getElementsByTagName("span")[k].getElementsByTagName("a")[0].textContent,
+											link: domain + info[j].getElementsByTagName("span")[k].getElementsByTagName("a")[0].href
+										});
+										break;
+									}
+									case "Thể Loại: ": {
+										for (var k = 0; k < info[j].getElementsByTagName("span").length; k++) {
+											var tagLink = info[j].getElementsByTagName("span")[k].getElementsByTagName("a")[0].href, name = tagLink.substr(tagLink.indexOf("the-loai-") + 9);
+											comicData.tags.push({
+												name: info[j].getElementsByTagName("span")[k].textContent,
+												link: domain + info[j].getElementsByTagName("span")[k].getElementsByTagName("a")[0].href,
+												tag: name.substr(0, name.indexOf(".html"))
+											});
+										}
+										break;
+									}
+								}
+							}
+							data.uploaded_comics.push(comicData);
+						}
+						break;
+					}
+					break;
+				}
+			}
+			var comments = document.getElementById("top-newest-comment").getElementsByTagName("article");
+			for (var i = 0; i < comments.length; i++) {
+				var comment = comments[i], comicLink = comment.getElementsByTagName("i")[0].getElementsByTagName("a")[0].href.substr(1);
+				data.latest_comments.push({
+					time: comment.getElementsByTagName("i")[0].textContent.substr(1, comment.getElementsByTagName("i")[0].textContent.indexOf(comment.getElementsByTagName("i")[0].getElementsByTagName("a")[0].textContent.split(" ")[0]) - 1),
+					comic: {
+						name: comment.getElementsByTagName("i")[0].getElementsByTagName("a")[0].textContent,
+						link: domain + comment.getElementsByTagName("i")[0].getElementsByTagName("a")[0].href,
+						id: parseInt(comicLink.substr(0, comicLink.indexOf("-")))
+					},
+					content: comment.getElementsByTagName("p")[0].textContent
+				});
+			}
+		}
+		catch (err) {
+			console.error(err);
+			return res.status(500).send({success: false, error_code: 500, message: err.toString(), url: url, documentation_url: "https://www.github.com/LilShieru/Touwa/wiki"});
+		}
+		// Returning the normal status
+		return res.send({success: true, url: url, ping: ping + "ms", data: data});
+	});
+});
+
 /*
 -----------------------------------------------------------------
 	Error handling
